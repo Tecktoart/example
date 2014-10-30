166656b3f1ae03413552f71839cf923aeff706cd
Артём <1987623@mail.ru>
2014-10-30 16:18:19 +0300

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+
+  has_many :articles, dependent: :destroy
+
+
+
+end
